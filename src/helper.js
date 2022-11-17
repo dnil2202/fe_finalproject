e361a5c1311e4b710cddcb0563364d966abbc926
dnil2202 <dnilsibarani@gmail.com>
2022-11-17 20:41:31 +0700

    cek

--- src/helper.js
+++ src/helper.js
@@ -1,2 +1,2 @@
-// export const API_URL = "http://localhost:4000"
-export const API_URL = "http://socmed-jambul.cvbuilder.id"
+export const API_URL = "http://localhost:4000"
+// export const API_URL = "http://socmed-jambul.cvbuilder.id"
